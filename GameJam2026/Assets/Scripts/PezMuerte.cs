e6e01f307dde585fab00069b04bb60350e9bd239
AlejandroEtayo <alejandro_etayo@euneiz.com>
2026-03-10 13:07:44 +0100

    añadida la parte del contenedor

---
+++ GameJam2026/Assets/Scripts/PezMuerte.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PezMuerte : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int VidaPez = 100;
+
+    public int DaniodeAcido = 200;
+
+
+    public static bool Pezmuerto;
+    void Start()
+    {
+        Pezmuerto = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

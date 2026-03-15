635a750e2c4710ec1434a3a1973eae7557a2d3aa
AlejandroEtayo <alejandro_etayo@euneiz.com>
2026-03-15 17:57:04 +0100

    S

--- GameJam2026/Assets/Scripts/Plataforma temporal.cs
+++ GameJam2026/Assets/Scripts/Plataforma temporal.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using Unity.Collections;
+using Unity.Collections;
 
 public class Plataformatemporal : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private float tiempoespera;
+
+    private Rigidbody2D rb2D;
+
+    [SerializeField] private float velocidadrotacion;
+
+    private Animator animator;
+    private bool caida = false;
+
+    private void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Caida(other));
+        }
+
+
+
+    }
+    private IEnumerator Caida(Collision2D other)
+    {
+        yield return new WaitForSeconds(tiempoespera);
+        caida = true;
+        Physics2D.IgnoreCollision(transform.GetComponent<Collider2D>(), other.transform.GetComponent<Collider2D>());
+        rb2D.constraints = RigidbodyConstraints2D.None;
+        rb2D.AddForce(new Vector2(0.1f, 0));
     }
 }
